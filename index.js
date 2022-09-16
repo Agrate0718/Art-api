ed3becac51c74a29c8b1082c07dad55f3ade2cc0
Agrate071818 <zionagrate@gmail.com>
2022-09-16 14:55:53 -0400

    Update

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+// required packages
 require('dotenv').config()
 const express = require('express')
 const ejsLayouts = require('express-ejs-layouts')
 const cookieParser = require('cookie-parser')
-// const db = require('./models')
+const db = require('../Art-api/models')
 const crypto = require('crypto-js')
 
-
+console.log('server secret:', process.env.ENC_SECRET)
 
 // config express app/middlewares
 const app = express()
@@ -14,11 +15,38 @@ app.set('view engine', 'ejs')
 app.use(ejsLayouts)
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-
-
-app.get('/', (req, res) => {
-    res.render('home.ejs')
-    console.log('welcome home')
+// our custom auth middleware
+app.use(async (req, res, next) => {
+    // console.log('hello from a middleware 👋')
+    // if there is a cookie on the incoming request
+    if (req.cookies.userId) {
+        // decrypt the user id before we look up the user in the db
+        const decryptedId = crypto.AES.decrypt(req.cookies.userId.toString(), process.env.ENC_SECRET)
+        const decryptedIdString = decryptedId.toString(crypto.enc.Utf8)
+        // look up the user in the db
+        const user = await db.user.findByPk(decryptedIdString)
+        // mount the user on the res.locals
+        res.locals.user = user
+    // if there is no cookie -- set the user to be null in the res.locals
+    } else {
+        res.locals.user = null
+    }
+    // move on to the next route or middleware in the chain
+    next()
 })
+
+
+
+// route definitions
+app.get('/', (req, res) => {
+    // console.log('incoming cookie 🍪', req.cookies)
+    // console.log(res.locals.myData)
+    console.log('the currently logged in user is:', res.locals.user)
+    res.render('home.ejs')
+})
+
+// Controllers
+app.use('/users', require('../Art-api/controllers/users'))
+
 // listen on a port
-app.listen(PORT, () => console.log(`Welcome to port ${PORT}`))
+app.listen(PORT, () => console.log(`you or your loved ones may be entitled to compensation on port: ${PORT}`))
